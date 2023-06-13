8233d3ebb63ee41487ac6e28613922addf10b56c
Ángel Guerrero Serrano <113890447+angelguerreroserrano@users.noreply.github.com>
2023-06-13 15:24:16 +0200

    Update and rename Ejercicio 3 - Hola "nombre".py to Ejercicio 3 - Hola "nombre"

--- Ejercicio 3 - Hola "nombre".py
+++
@@ -1,6 +0,0 @@
-# Escribir un programa que pregunte el nombre del usuario en la consola y después
-  de que el usuario lo introduzca muestre por pantalla la cadena ¡Hola <nombre>!,
-  donde <nombre> es el nombre que el usuario haya introducido.
-
-nom = input("Dime tu nombre: ")
-print (f"¡Hola {nom}!")
